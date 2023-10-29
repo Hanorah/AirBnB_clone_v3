506820c5640c34bef38be5107b60abec029075f1
OkosodoNorah <ksdnorah@gmail.com>
2023-10-29 10:40:07 -0700

    Add contents of web_static/AirBnB_clone_v3/

---
+++ web_static/api/v1/views/index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+App view for AirBnB_clone_v3
+"""
+
+from flask import jsonify
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """ returns status """
+    status = {"status": "OK"}
+    return jsonify(status)
+
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    """ returns number of each objects by type """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
